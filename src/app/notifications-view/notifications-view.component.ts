a7f9f5ae51acac392eaf9667e5bb08ee1a9c69f3
liaud1u <alexis.liaud7@etu.univ-lorraine.fr>
2021-12-01 17:39:30 +0100

    Notifications view documentation and refactoring.

--- src/app/notifications-view/notifications-view.component.ts
+++ src/app/notifications-view/notifications-view.component.ts
@@ -11,24 +11,48 @@ import {StorageService} from "../shared/services/storage.service";
   styleUrls: ['./notifications-view.component.css']
 })
 export class NotificationsViewComponent implements OnInit {
+
+  /// List of all notifications of the user
+  private _notifications: Notification[];
+
+  /**
+   * Constructor of the notification view
+   *
+   * @param _dialogRef {MatDialogRef} reference of the dialog
+   * @param _notificationService {NotificationsService} service managing notifications
+   * @param _storageService {StorageService} service managing storage of tokens and users
+   * @param _data {{ notifications: Notification[] }} list of all the notifications gived by the reference component
+   */
   constructor(private _dialogRef: MatDialogRef<NavbarComponent>, private _notificationService: NotificationsService, private _storageService: StorageService, @Inject(MAT_DIALOG_DATA) private _data: { notifications: Notification[] }) {
     this._notifications = _data.notifications;
   }
 
-  private _notifications: Notification[];
-
+  /**
+   * Getter of the list of all notifications
+   *
+   * @return {Notifications[]}
+   */
   get notifications(): Notification[] {
     return this._notifications;
   }
 
+  /**
+   * Setter of the list of notifications
+   *
+   * @param value {Notification[]} list of all notifications
+   */
   set notifications(value: Notification[]) {
     this._notifications = value;
   }
 
-  ngOnInit(): void {
+  /**
+   * On init implementation
+   */
+  ngOnInit(): void {}
 
-  }
-
+  /**
+   * Close the dialog
+   */
   close() {
     this._dialogRef.close();
   }
