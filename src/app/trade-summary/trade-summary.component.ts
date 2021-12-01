caa3fabd4877928a540e53e259855edb4184fae1
liaud1u <alexis.liaud7@etu.univ-lorraine.fr>
2021-12-01 22:48:15 +0100

    Trade summary, trades, trades offers, update form documentation and refactoring.

--- src/app/trade-summary/trade-summary.component.ts
+++ src/app/trade-summary/trade-summary.component.ts
@@ -10,23 +10,44 @@ import {Trade} from "../shared/types/trade.type";
 })
 export class TradeSummaryComponent implements OnInit {
 
+  /// Trade to display in the summary component
+  private _trade: Trade;
+
+  /**
+   * Constructor of the trade summary
+   *
+   * @param _dialogRef {MatDialogRef<CardsTradeComponent>} Reference to the parent
+   * @param _data  {{ trade: Trade }} Data gived by the parent
+   */
   constructor(private _dialogRef: MatDialogRef<CardsTradeComponent>, @Inject(MAT_DIALOG_DATA) private _data: { trade: Trade }) {
     this._trade = _data.trade;
   }
 
-  private _trade: Trade;
-
+  /**
+   * Getter of the trade
+   *
+   * @return {Trade}
+   */
   get trade(): Trade {
     return this._trade;
   }
 
+  /**
+   * On init implementation
+   */
   ngOnInit(): void {
   }
 
+  /**
+   * Method to close the dialog and cancel the trade
+   */
   redirectBack() {
     this._dialogRef.close();
   }
 
+  /**
+   * Method to purpose the dialog and purpose the trade by giving the trade to parent
+   */
   purpose() {
     this._dialogRef.close(this._trade);
   }
--- src/app/update-form/update-form.component.ts
+++ src/app/update-form/update-form.component.ts
@@ -12,13 +12,39 @@ import {CustomValidators} from "../shared/validators/custom-validators";
 })
 export class UpdateFormComponent implements OnInit {
 
+  /// User currently updating him informations
+  private _user: User;
+
+  /// Boolean variable to know if the password need to be hide or not
+  private _hide: boolean;
+
+  /// Boolean variable to know if an error need to be displayed
+  private _error: boolean;
+
+  /// FormGroup used to updates users informations
+  private _updateForm: FormGroup;
+
+  /**
+   * Constructor of the update form
+   *
+   * @param _dialogRef {MatDialogRef<LoginComponent>} reference to the parent dialogue
+   * @param _data {{ error: boolean, user: User }} dates provided by the parent, error: true if the form need to display error, user to use to fill form
+   */
   constructor(private _dialogRef: MatDialogRef<LoginComponent>, @Inject(MAT_DIALOG_DATA) private _data: { error: boolean, user: User }) {
     this._user = _data.user;
     this._error = _data.error;
     this._hide = true;
+    this._updateForm = this._buildUpdateForm()
+  }
 
-
-    this._updateForm = new FormGroup({
+  /**
+   * Method to build the form
+   * @private
+   *
+   * @return {FormGroup}
+   */
+  private _buildUpdateForm(): FormGroup{
+    return new FormGroup({
       firstname: new FormControl('', Validators.compose([Validators.required, Validators.minLength(2)])),
       lastname: new FormControl('', Validators.compose([Validators.required, Validators.minLength(2)])),
       username: new FormControl('', Validators.compose([Validators.required, Validators.minLength(2)])),
@@ -28,53 +54,60 @@ export class UpdateFormComponent implements OnInit {
     }, {validators: CustomValidators.passwordDif("password", "passwordConfirm")});
   }
 
-  private _user: User;
-
+  /**
+   * Getter of the user to update
+   *
+   * @return {User}
+   */
   get user(): User {
     return this._user;
   }
 
-  set user(value: User) {
-    this._user = value;
-  }
-
-  private _hide: boolean;
-
+  /**
+   * Getter of the hide boolean
+   *
+   * @return {boolean} true if the password need to be hide, false either
+   */
   get hide(): boolean {
     return this._hide;
   }
 
-  set hide(value: boolean) {
-    this._hide = value;
-  }
-
-  private _error: boolean;
-
+  /**
+   * Getter of the error boolean
+   *
+   * @return {boolean} true if an error need to be displayed in the form, false either
+   */
   get error(): boolean {
     return this._error;
   }
 
-  set error(error: boolean) {
-    this._error = error;
-  }
-
-  private _updateForm: FormGroup;
-
+  /**
+   * Getter to get the updateForm
+   *
+   * @return {FormGroup}
+   */
   get updateForm(): FormGroup {
     return this._updateForm;
   }
 
-  set updateForm(value: FormGroup) {
-    this._updateForm = value;
-  }
-
+  /**
+   * On init implementation
+   */
   ngOnInit(): void {
   }
 
+  /**
+   * Method wich close the update dialog if the user choose to cancel the update
+   */
   redirectBack() {
     this._dialogRef.close();
   }
 
+  /**
+   * Method wich close the update dialog and send the new user
+   *
+   * @param user {User} new users informations
+   */
   update(user: User) {
     this._dialogRef.close(user);
   }
