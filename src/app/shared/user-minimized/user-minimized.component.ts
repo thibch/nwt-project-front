172e6963544653e8d0dacbef36d32546e0bd8e2a
liaud1u <alexis.liaud7@etu.univ-lorraine.fr>
2021-12-01 22:27:44 +0100

    Trade, user cards, user minimized documentation and refactoring.

--- src/app/shared/user-minimized/user-minimized.component.ts
+++ src/app/shared/user-minimized/user-minimized.component.ts
@@ -8,21 +8,38 @@ import {User} from "../types/user.type";
 })
 export class UserMinimizedComponent implements OnInit {
 
+  /// User to display profil in minimized
+  private _user: User;
+
+  /**
+   * Constructor of the minimized profile component
+   */
   constructor() {
     this._user = {} as User;
   }
 
-  private _user: User;
-
+  /**
+   * Getter of the minimized user
+   *
+   * @return {User}
+   */
   get user(): User {
     return this._user;
   }
 
+  /**
+   * Setter of the minimized user
+   *
+   * @param value {User} user to display in minimized
+   */
   @Input('user')
   set user(value: User) {
     this._user = value;
   }
 
+  /**
+   * On init implementation
+   */
   ngOnInit(): void {
   }
 }
