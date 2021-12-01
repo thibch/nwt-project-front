499f212398cb5a1a4537333ba553cb1ff945e131
liaud1u <alexis.liaud7@etu.univ-lorraine.fr>
2021-12-01 17:46:25 +0100

    Public profile documentation and refactoring.

--- src/app/public-profile/public-profile.component.ts
+++ src/app/public-profile/public-profile.component.ts
@@ -15,50 +15,84 @@ import {StorageService} from "../shared/services/storage.service";
   styleUrls: ['./public-profile.component.css']
 })
 export class PublicProfileComponent implements OnInit {
+
+  /// List of all collections of the user
+  private _collections: Collection[];
+
+  /// User matching this profile
+  private _user: User;
+
+  /**
+   * Constructor of an user profile
+   *
+   * @param _route {ActivatedRoute} used to get the id of the user displayed
+   * @param _storageService {StorageService} service managing storage of tokens and users
+   * @param _userService {UserService} service managing users
+   * @param _collectionService {CollectionService} service managing collections
+   * @param _cardService {CardService} service managing cards
+   */
   constructor(private _route: ActivatedRoute, private _storageService: StorageService, private _userService: UserService, private _collectionService: CollectionService, private _cardService: CardService) {
     this._user = {} as User;
     this._collections = [];
   }
 
-  private _user: User;
-
+  /**
+   * Getter of the user of this profile
+   *
+   * @return {User}
+   */
   get user(): User {
     return this._user;
   }
 
+  /**
+   * Setter of the user
+   *
+   * @param value {User}
+   */
   @Input('user')
   set user(value: User) {
     this._user = value;
   }
 
-  private _collections: Collection[];
-
+  /**
+   * Getter of the list of collections
+   *
+   * @return {Collection[]}
+   */
   get collections(): Collection[] {
     return this._collections;
   }
 
+  /**
+   * Setter of the list of collections
+   *
+   * @param value {Collection[]}
+   */
   set collections(value: Collection[]) {
     this._collections = value;
   }
 
-  public isMyAccount() {
-    return this._user.id == this._storageService.getUser().id;
-  }
-
+  /**
+   * On init implementation
+   */
   ngOnInit(): void {
+    // Get the user matching the route id
     merge(
       this._route.params.pipe(
         filter((params: any) => !!params.id),
         mergeMap((params: any) => this._userService.fetchOne(params.id)),
       )
     ).subscribe({
-      next: (user: User) => this._user = user,
+      next: (user: User) => {
+        this._user = user
+      },
       error: () => {
         this._user = {} as User;
       }
     });
 
-
+    // Get all collections matching the user id
     merge(
       this._route.params.pipe(
         filter((params: any) => !!params.id),
