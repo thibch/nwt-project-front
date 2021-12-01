ab377169a9a3e7bab68f0b814dae756b022ed814
liaud1u <alexis.liaud7@etu.univ-lorraine.fr>
2021-12-01 17:35:23 +0100

    Login documentation and refactoring.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,22 +14,41 @@ import {StorageService} from "../shared/services/storage.service";
 })
 export class LoginComponent implements OnInit {
 
+  /// Mat dialog to open the Login form
   private _loginDialog: MatDialogRef<LoginFormComponent, any>;
+
+  /// User currently logging in
   private _loggedUser: User | undefined;
+
+  /// Boolean to print error if authentification fail
   private _error: boolean;
 
-  constructor(private _router: Router, private _dialog: MatDialog, private _errorDialog: MatDialog, private _loginService: LoginService, private _storageService: StorageService) {
+  /**
+   * Constructor of the login component
+   *
+   * @param _router {Router} router for the redirections
+   * @param _dialog {MatDialog} mat dialog where the form will be displayed
+   * @param _loginService {LoginService} service managing login
+   * @param _storageService {StorageService} service managing storage of tokens and users
+   */
+  constructor(private _router: Router, private _dialog: MatDialog, private _loginService: LoginService, private _storageService: StorageService) {
     this._loginDialog = {} as MatDialogRef<LoginFormComponent, any>;
     this._loggedUser = {} as User;
     this._error = false;
   }
 
+  /**
+   * On Init implemenation
+   */
   ngOnInit(): void {
     this._error = false;
     this._openDialog()
   }
 
-
+  /**
+   * Open the authentification login
+   * @private
+   */
   private _openDialog() {
     // create modal with initial data inside
     this._loginDialog = this._dialog.open(LoginFormComponent, {
@@ -38,6 +57,7 @@ export class LoginComponent implements OnInit {
       data: {error: this._error, user: this._loggedUser}
     });
 
+    // When the dialog is closed
     this._loginDialog.afterClosed()
       .pipe(
         filter((user: User | undefined) => !!user),
@@ -48,6 +68,8 @@ export class LoginComponent implements OnInit {
         mergeMap((user: User | undefined) => this._loginService.auth(user as User)),
       ).subscribe(
       data => {
+        // If login succeed, we save the user in local storage with the token
+
         this._storageService.saveUser({
           username: data.username,
           email: data.email,
@@ -60,10 +82,12 @@ export class LoginComponent implements OnInit {
         });
         this._storageService.saveToken(data.access_token);
 
+        // Redirect back to home
         this._router.navigate(['/home'])
         this._error = false;
       },
       error => {
+        // On error, we open the dialog with an error
         this._error = true;
         this._openDialog()
       });
