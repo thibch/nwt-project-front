dc5c40e26c6cb331dc1bab11fe0b62574403df41
liaud1u <alexis.liaud7@etu.univ-lorraine.fr>
2021-12-01 17:27:32 +0100

    Delete trade documentation and refactoring.

--- src/app/delete/delete.component.ts
+++ src/app/delete/delete.component.ts
@@ -15,31 +15,52 @@ import {AuthFormComponent} from "../shared/auth-form/auth-form.component";
 })
 export class DeleteComponent implements OnInit {
 
+  /// Dialog for the autenthification before deleting the account
   private _deleteDialog: MatDialogRef<AuthFormComponent, User>;
-  private _loggedUser: User | undefined;
+
+  /// User wich is currently logged
+  private _loggedUser: User ;
+
+  /// Boolean, used to display an error in the auth form if auth failed
   private _error: boolean;
 
-  constructor(private _router: Router, private _dialog: MatDialog, private _errorDialog: MatDialog, private _loginService: LoginService, private _storageService: StorageService, private _userService: UserService) {
+  /**
+   * Constructor of the delete component wich open an authentification dialog
+   *
+   * @param _router {Router} router used for the redirections
+   * @param _dialog {MatDialog} mat dialog used to open the authentification form
+   * @param _loginService {LoginService} service managing login
+   * @param _storageService {StorageService} service managing the storage
+   * @param _userService {UserService} service managing users
+   */
+  constructor(private _router: Router, private _dialog: MatDialog, private _loginService: LoginService, private _storageService: StorageService, private _userService: UserService) {
     this._deleteDialog = {} as MatDialogRef<AuthFormComponent, User>;
+    this._loggedUser = {} as User;
     this._error = false;
   }
 
+  /**
+   * On init implementation
+   */
   ngOnInit(): void {
     this._loggedUser = this._storageService.getUser();
     this._error = false;
     this._openDialog()
   }
 
-
+  /**
+   * Method wich open the authentification form to check user password before deleting the account
+   * @private
+   */
   private _openDialog() {
-
+    // Creating the form
     this._deleteDialog = this._dialog.open(AuthFormComponent, {
       width: '400px',
       disableClose: true,
       data: {error: this._error, user: this._loggedUser, operation: "suppression"}
     });
 
-
+    // When the form is closed
     this._deleteDialog.afterClosed()
       .pipe(
         filter((user: User | undefined) => !!user),
@@ -47,7 +68,7 @@ export class DeleteComponent implements OnInit {
         mergeMap((user: User) => this._loginService.auth(user)),
       ).subscribe(
       data => {
-
+        // If authentification succeed, we delete the account
         this._userService.delete(data.id).subscribe(
           data => {
             this._storageService.logout();
@@ -60,6 +81,7 @@ export class DeleteComponent implements OnInit {
           });
       },
       error => {
+        // Else, we print an error and reopen the dialog
         this._error = true;
         this._openDialog();
       });
